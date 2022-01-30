baf01dbdddaaa7e09336a753e1de700f3575a218
jmcabreira <jm_cabreira@id.uff.br>
2022-01-30 08:30:20 -0300

    feat: Add clean_data.py file

---
+++ starter/starter/clean_data.py
@@ -0,0 +1,32 @@
+
+import sys
+from pathlib import Path
+import yaml
+from box import Box
+import os
+
+STARTER_ROOT = str(Path(__file__).resolve().parents[1])
+# Adds cwd path to the sys.path list in the first position so 
+# that python will search the cwd first 
+sys.path.insert(0,STARTER_ROOT)
+
+# Project Directories
+from ml.data import clean_raw_data
+CONFIG_FILEPATH = os.path.join(STARTER_ROOT, "config.yaml")
+
+# Load config file
+with open(CONFIG_FILEPATH, "r", encoding="utf-8") as ymlfile:
+  config = Box(yaml.safe_load(ymlfile))
+
+RAW_DATA_FILE_PTH = os.path.join(STARTER_ROOT,config.data.raw.filepath)
+CLEANED_DATA_FILE_PTH = os.path.join(STARTER_ROOT, config.data.cleaned.filepath)
+
+print(RAW_DATA_FILE_PTH)
+print(CLEANED_DATA_FILE_PTH)
+
+def run_clean_data():
+  cleaned_df = clean_raw_data(filepath = RAW_DATA_FILE_PTH)
+  cleaned_df.to_csv(CLEANED_DATA_FILE_PTH)
+
+if __name__ == '__main__':
+  run_clean_data()

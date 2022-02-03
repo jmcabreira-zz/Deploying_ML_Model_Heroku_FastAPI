537b9c7c6b7fdfd2d39b9d07cef0f49292cf8368
jmcabreira <jm_cabreira@id.uff.br>
2022-02-02 22:22:52 -0300

    feat: add new training job - after fixing the train_model script

--- starter/model_card_template.md
+++ starter/model_card_template.md
@@ -1,18 +1,56 @@
 # Model Card
 
-For additional information see the Model Card paper: https://arxiv.org/pdf/1810.03993.pdf
+GradientBoostingClassifier trained on the [US census dataset](https://archive.ics.uci.edu/ml/datasets/census+income).
 
 ## Model Details
 
+- The model: `GradientBoostingClassifier`
+- Train: `80% training data`
+- Test: `20% for testing data`
+
 ## Intended Use
+
+The model aims to predict whether a person earns more than $50k based on census information.
 
 ## Training Data
 
+The data: [US census dataset](https://archive.ics.uci.edu/ml/datasets/census+income).
+The data was splited: 80% training and 20% testing
+Categorical features :
+
+- `workclass`
+- `education`
+- `marital_status`
+- `occupation`
+- `relationship`
+- `race`
+- `sex`
+- `native_country`
+
 ## Evaluation Data
 
+I used 20% of the data to evaluate the model
+
 ## Metrics
-_Please include the metrics used and your model's performance on those metrics._
+
+Using all data:
+
+- train:
+
+* `precision`: 79%
+* `recall`: 61%
+* `fbeta`: 69%
+
+- test:
+
+* `precision`: 79%
+* `recall`: 59%
+* `fbeta`: 67%
 
 ## Ethical Considerations
 
+The Dataset contains data related race, gender and origin country. This may drive to a model that discriminate people and because of that, further investigation should be done before use it.
+
 ## Caveats and Recommendations
+
+The model could be improved by analysing feature importance and performing feature engineering on the dataset.
--- starter/starter/train_model.py
+++ starter/starter/train_model.py
@@ -9,7 +9,7 @@ import os
 import json
 import yaml
 from box import Box
-import pandas as pd 
+import pandas as pd
 from ml.data import process_data
 from ml.model import train_model, inference, compute_model_metrics
 from sklearn.model_selection import train_test_split
@@ -20,70 +20,73 @@ import numpy as np
 
 # Prepare logging
 STARTER_ROOT = str(Path(__file__).resolve().parents[1])
-LOG_FOLDER = os.path.join(STARTER_ROOT,'logs')
+LOG_FOLDER = os.path.join(STARTER_ROOT, 'logs')
 CONFIG_FILEPATH = os.path.join(STARTER_ROOT, "config.yaml")
-LOG_FILE_PTH = os.path.join(LOG_FOLDER,'train_model.log')
+LOG_FILE_PTH = os.path.join(LOG_FOLDER, 'train_model.log')
 
 if not os.path.exists(LOG_FOLDER):
     os.makedirs(LOG_FOLDER)
 print(CONFIG_FILEPATH)
-with open(CONFIG_FILEPATH, 'r', encoding = 'UTF-8') as configfile:
+with open(CONFIG_FILEPATH, 'r', encoding='UTF-8') as configfile:
     config = Box(yaml.safe_load(configfile))
 
-CLEANED_DATA_FILEPATH = os.path.join(STARTER_ROOT,config.data.cleaned.filepath)
+CLEANED_DATA_FILEPATH = os.path.join(
+    STARTER_ROOT, config.data.cleaned.filepath)
 
 logging.basicConfig(
-        filename = LOG_FILE_PTH,
-            format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-            filemode='w',
-            level=logging.INFO
-                )   
+    filename=LOG_FILE_PTH,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    filemode='w',
+    level=logging.INFO
+)
+
 
 def run_train_model():
     logging.info('Train model log')
     # Add code to load in the data.
-    data = pd.read_csv(CLEANED_DATA_FILEPATH).drop(["Unnamed: 0"], axis = 1)
-    
+    data = pd.read_csv(CLEANED_DATA_FILEPATH).drop(["Unnamed: 0"], axis=1)
+
     # Optional enhancement, use K-fold cross validation instead of a train-test split.
-    train, test = train_test_split(data, 
-                                test_size=config.preprocessing.test_size,
-                                random_state=config.model.random_seed)
+    train, test = train_test_split(data,
+                                   test_size=config.preprocessing.test_size,
+                                   random_state=config.model.random_seed)
 
-
-    # Proces the train data with the process_data function.  
+    # Proces the train data with the process_data function.
     X_train, y_train, encoder, lb = process_data(
-        train, categorical_features= config.preprocessing.cat_features, 
-                                    label = config.preprocessing.label,
-                                    training = True
+        train, categorical_features=config.preprocessing.cat_features,
+        label=config.preprocessing.label,
+        training=True
     )
 
     # Proces the test data with the process_data function.
     X_test, y_test, encoder, lb = process_data(
-        train, categorical_features= config.preprocessing.cat_features,
-                                     label = config.preprocessing.label, 
-                                     training=False,
-                                     encoder = encoder,
-                                     lb=lb
+        test, categorical_features=config.preprocessing.cat_features,
+        label=config.preprocessing.label,
+        training=False,
+        encoder=encoder,
+        lb=lb
     )
 
     # Train the model
     model = train_model(X_train, y_train, config)
 
     # Save model and encoder
-    pickle.dump(model, open(os.path.join(STARTER_ROOT,config.model.GradientBoostingClassifier.output_file_pth) , "wb"))
-    pickle.dump(encoder, open(os.path.join(STARTER_ROOT, config.preprocessing.encoder_file_path), "wb"))
-    pickle.dump(lb, open(os.path.join(STARTER_ROOT,config.preprocessing.binarizer_file_path), "wb"))
+    pickle.dump(model, open(os.path.join(
+        STARTER_ROOT, config.model.GradientBoostingClassifier.output_file_pth), "wb"))
+    pickle.dump(encoder, open(os.path.join(
+        STARTER_ROOT, config.preprocessing.encoder_file_path), "wb"))
+    pickle.dump(lb, open(os.path.join(
+        STARTER_ROOT, config.preprocessing.binarizer_file_path), "wb"))
 
     # Save processes data
-    with open(os.path.join(STARTER_ROOT,config.preprocessing.train_filepath), "wb") as f:
+    with open(os.path.join(STARTER_ROOT, config.preprocessing.train_filepath), "wb") as f:
         np.save(f, X_train)
         np.save(f, y_train)
 
-    with open(os.path.join(STARTER_ROOT,config.preprocessing.test_filepath), "wb") as f:
+    with open(os.path.join(STARTER_ROOT, config.preprocessing.test_filepath), "wb") as f:
         np.save(f, X_test)
         np.save(f, y_test)
 
+
 if __name__ == '__main__':
     run_train_model()
-
-    

d18d59573e0b98b1c5ee3d7ecf95afeb28a91346
jmcabreira <jm_cabreira@id.uff.br>
2022-02-04 23:40:03 -0300

    refact: fix test functions

--- starter/starter/tests/test_model.py
+++ starter/starter/tests/test_model.py
@@ -5,6 +5,15 @@ from pathlib import Path
 import yaml
 from box import Box
 import pandas as pd
+import sys
+import pytest
+
+ml_path = str(Path(__file__).resolve().parents[1])
+sys.path.insert(0, ml_path)
+
+import ml
+from ml.data import *
+from ml.model import *
 
 STARTER_ROOT = str(Path(__file__).resolve().parents[2])
 LOG_FOLDER = os.path.join(STARTER_ROOT, "logs")
@@ -13,11 +22,12 @@ LOG_FILE_PTH = os.path.join(LOG_FOLDER, "unit_test.log")
 
 if not os.path.exists(LOG_FOLDER):
     os.makedirs(LOG_FOLDER)
-print(CONFIG_FILEPATH)
+
 with open(CONFIG_FILEPATH, "r", encoding="UTF-8") as configfile:
     config = Box(yaml.safe_load(configfile))
 
 CLEANED_DATA_FILEPATH = os.path.join(STARTER_ROOT, config.data.cleaned.filepath)
+print(CLEANED_DATA_FILEPATH)
 
 
 @pytest.fixture
@@ -25,9 +35,7 @@ def data():
     """
     Load the cleaned dataset
     """
-    df = pd.read_csv(os.path.join(STARTER_ROOT, "data/census_cleaned.csv")).drop(
-        ["Unnamed: 0"], axis=1
-    )
+    df = pd.read_csv(CLEANED_DATA_FILEPATH).drop(["Unnamed: 0"], axis=1)
     return df
 
 
@@ -36,8 +44,8 @@ def test_dataframe_shape(data):
     test dataframe shape
     """
     try:
-        assert len(data.shape[0]) > 0
-        assert len(data.shape[1]) == 15
+        assert data.shape[0] > 0
+        assert data.shape[1] == 15
         logging.info(
             f"Testing the cleaned data: The cleaned dataset has {data.shape[0]} rows and {data.shape[1]} columns as expected."
         )
@@ -48,13 +56,14 @@ def test_dataframe_shape(data):
         raise e
 
 
-def test_columns_name(data):
+def test_column_names(data):
     """
     test the columns dataset name
     """
     right_columns = [
         "age",
         "workclass",
+        "fnlgt",
         "education",
         "education-num",
         "marital-status",
@@ -62,26 +71,28 @@ def test_columns_name(data):
         "relationship",
         "race",
         "sex",
+        "capital-gain",
+        "capital-loss",
         "hours-per-week",
         "native-country",
         "salary",
     ]
-
     data_columns = data.columns.values
     try:
         assert list(right_columns) == list(data_columns)
         logging.info("Testing Cleaned data: Column names are as expected")
     except AssertionError as e:
         logging.error(
-            f"Testing cleaned data: Column names are not as expected. Dataset columnsa: {data_columns}"
+            f"Testing cleaned data: Column names are not as expected. Dataset columns: {data_columns}"
         )
         raise e
 
 
 def test_missing_values(data):
-
+    print(data.isnull().sum())
     try:
-        assert data.isnull().sum() == 0
+        assert data.isnull().sum().sum() == 0
+        print(data)
         logging.info("Testing cleaned data: No rows with null values.")
     except AssertionError as e:
         logging.error(
@@ -100,3 +111,15 @@ def test_question_mark(data):
     except AssertionError as e:
         logging.error(f"Testing cleaned data: There is still ? in the clenaed data")
         raise e
+
+
+# def run_cleaned_tests():
+#     df = data()
+#     test_dataframe_shape(df)
+#     test_column_names(df)
+#     test_missing_values(df)
+#     test_question_mark(df)
+
+
+# if __name__ == "__main__":
+#     run_cleaned_tests()

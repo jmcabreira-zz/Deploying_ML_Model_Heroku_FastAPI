1eab882436d0b10b7dbe727ebd123057f9269c7d
jmcabreira <jm_cabreira@id.uff.br>
2022-02-09 23:14:23 -0300

    feat: add requirements

--- starter/test_api/test_api.py
+++ starter/test_api/test_api.py
@@ -25,29 +25,6 @@ def test_welcome(client):
     assert req.json() == {"Greeting": "Hello and Welcome :)"}, "Wrong json output"
 
 
-# def test_below_50k_pred(client):
-#     data = {
-#         "age": 39,
-#         "workclass": "State-gov",
-#         "fnlgt": 77516,
-#         "education": "Bachelors",
-#         "education_num": 13,
-#         "marital_status": "Never-married",
-#         "occupation": "Adm-clerical",
-#         "relationship": "Not-in-family",
-#         "race": "White",
-#         "sex": "Male",
-#         "capital_gain": 2020,
-#         "capital_loss": 0,
-#         "hours_per_week": 50,
-#         "native_country": "United-States",
-#     }
-
-#     response = client.post("/predict/", json=data)
-#     assert response.status_code == 200, "Status code is not 200"
-#     assert response.json() == {"prediction": ">50K"}, "Wrong prediction"
-
-
 def test_below_50k_pred(client):
     input_dict = {
         "age": 39,
